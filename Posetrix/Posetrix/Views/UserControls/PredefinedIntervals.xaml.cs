65ed612f4cf2726366fcd8b6a51a3f484ba9441d
thoebbish <40149294+thoebbish@users.noreply.github.com>
2024-11-16 20:54:30 +0300

    style: fix custom contols styling

---
+++ Posetrix/Posetrix/Views/UserControls/PredefinedIntervals.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Posetrix.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PredefinedTimes.xaml
+    /// </summary>
+    public partial class PredefinedIntervals : UserControl
+    {
+        public PredefinedIntervals()
+        {
+            InitializeComponent();
+        }
+    }
+}
