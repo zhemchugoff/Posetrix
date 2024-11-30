5288e660c3193ae193a3d9a3985ef0b4bf54b516
thoebbish <40149294+thoebbish@users.noreply.github.com>
2024-11-30 20:01:27 +0300

    feat(avalonia): add theme toggle

--- Posetrix.Avalonia/Views/SettingsWindow.axaml.cs
+++ Posetrix.Avalonia/Views/SettingsWindow.axaml.cs
@@ -1,6 +1,7 @@
-﻿using Avalonia;
+﻿using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 
 namespace Posetrix.Avalonia.Views;
 
@@ -9,5 +10,24 @@ public partial class SettingsWindow : Window
     public SettingsWindow()
     {
         InitializeComponent();
+    }
+
+    private void ChangeTheme_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems.Count > 0 && (ComboBoxItem)e.AddedItems[0] is not null)
+        {
+            ComboBoxItem selectedItem = (ComboBoxItem)e.AddedItems[0];
+
+            string selectedValue = selectedItem.Content.ToString();
+
+            if (Application.Current == null) return;
+            Application.Current.RequestedThemeVariant = selectedValue switch
+            {
+                "Light" => ThemeVariant.Light,
+                "Dark" => ThemeVariant.Dark,
+                "System" => ThemeVariant.Default,
+                _ => Application.Current.RequestedThemeVariant
+            };
+        }
     }
 }
