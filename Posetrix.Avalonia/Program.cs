fb7ab9568c188f7b45fac11a17f3ce7fad1587bc
thoebbish <40149294+thoebbish@users.noreply.github.com>
2024-11-30 17:41:43 +0300

    style(avalonia): add icon pack for dark and light themes

--- Posetrix.Avalonia/Program.cs
+++ Posetrix.Avalonia/Program.cs
@@ -13,8 +13,8 @@ sealed class Program
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
