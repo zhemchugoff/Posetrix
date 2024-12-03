d1b7d9af5ea2251a8bd45bc49043d84d593942e7
thoebbish <40149294+thoebbish@users.noreply.github.com>
2024-12-03 18:42:56 +0300

    fix(avalonia): fix image rotation from EXIF

--- Posetrix.Avalonia/Converters/SkiaSharpImageLoader.cs
+++ Posetrix.Avalonia/Converters/SkiaSharpImageLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using Avalonia.Data.Converters;
@@ -9,7 +10,8 @@ namespace Posetrix.Avalonia.Converters;
 
 public class SkiaSharpImageLoader : IValueConverter
 {
-    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is string imagePath && !string.IsNullOrEmpty(imagePath))
         {
@@ -19,7 +21,7 @@ public class SkiaSharpImageLoader : IValueConverter
 
         return null;
     }
-    
+
     /// <summary>
     /// Method <c>LoadImage</c> loads an image using SkiaSharp, resizes it  and converts to Avalonia bitmap.
     /// </summary>
@@ -27,26 +29,28 @@ public class SkiaSharpImageLoader : IValueConverter
     {
         // Load the original image.
         using var skBitmap = SKBitmap.Decode(imagePath);
-        
+        var orientation = GetImageOrientation(imagePath);
+        var rotetedSkBitmap = AutoOrient(skBitmap, orientation);
+
         // Calculate new dimensions.
-        var (newWidth, newHeight) = CalculateNewDimensions(skBitmap.Width, skBitmap.Height, maxWidth, maxHeight);
-        
-        // TODO: fix image rotation from EXIF metadata or XMP metadata
-        
+        var (newWidth, newHeight) =
+            CalculateNewDimensions(rotetedSkBitmap.Width, rotetedSkBitmap.Height, maxWidth, maxHeight);
+
+        // TODO: add cache for images.
+
         // Resize the image.
-        using var resizedBitmap = skBitmap.Resize(
+        using var resizedBitmap = rotetedSkBitmap.Resize(
             new SKImageInfo(newWidth, newHeight),
             SKFilterQuality.Medium
         );
-        
+
         if (resizedBitmap == null)
             throw new Exception("Image resizing failed.");
-        
+
         // Convert to Avalonia Bitmap.
         using var skImage = SKImage.FromBitmap(resizedBitmap);
         using var data = skImage.Encode(SKEncodedImageFormat.Jpeg, 100);
         using var stream = new MemoryStream(data.ToArray());
-        
         // Return the resized Avalonia bitmap.
         return new Bitmap(stream);
     }
@@ -54,12 +58,8 @@ public class SkiaSharpImageLoader : IValueConverter
     /// <summary>
     /// Method <c>CalculateNewDimensions</c> calculates the new dimensions using the smaller scale factor to preserve the aspect ratio.
     /// </summary>
-    /// <param name="originalWidth"></param>
-    /// <param name="originalHeight"></param>
-    /// <param name="maxWidth"></param>
-    /// <param name="maxHeight"></param>
-    /// <returns></returns>
-    private static (int Width, int Height) CalculateNewDimensions(int originalWidth, int originalHeight, int maxWidth, int maxHeight)
+    private static (int Width, int Height) CalculateNewDimensions(int originalWidth, int originalHeight, int maxWidth,
+        int maxHeight)
     {
         // Calculate scaling factors for width and height
         double widthScale = (double)maxWidth / originalWidth;
@@ -74,9 +74,60 @@ public class SkiaSharpImageLoader : IValueConverter
 
         return (newWidth, newHeight);
     }
-    
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+
+    /// <summary>
+    /// Method <c>GetImageOrientation</c> returns an orientation of an encoded image from EXIF data.
+    /// </summary>
+    public static SKEncodedOrigin GetImageOrientation(string imagePath)
     {
-        throw new NotImplementedException();
+        using (var stream = File.OpenRead(imagePath))
+        {
+            var codec = SKCodec.Create(stream);
+            return codec.EncodedOrigin;
+        }
     }
-}
+
+    /// <summary>
+    /// Method <c>AutoOrient</c> rotates an image according to EXIF data.
+    /// </summary>
+    private static SKBitmap AutoOrient(SKBitmap bitmap, SKEncodedOrigin origin)
+    {
+        // No transformation needed.
+        if (origin == SKEncodedOrigin.TopLeft)
+        {
+            return bitmap;
+        }
+
+        SKBitmap rotatedBitmap = new SKBitmap(bitmap.Height, bitmap.Width);
+
+        using (var canvas = new SKCanvas(rotatedBitmap))
+        {
+            switch (origin)
+            {
+                case SKEncodedOrigin.BottomRight:
+                    canvas.Translate(bitmap.Width, bitmap.Height);
+                    canvas.RotateDegrees(180);
+                    break;
+                case SKEncodedOrigin.RightTop:
+                    canvas.Translate(bitmap.Height, 0);
+                    canvas.RotateDegrees(90);
+                    break;
+                case SKEncodedOrigin.LeftBottom:
+                    canvas.Translate(0, bitmap.Width);
+                    canvas.RotateDegrees(270);
+                    break;
+                default: return bitmap;
+            }
+
+            canvas.DrawBitmap(bitmap, 0, 0);
+        }
+
+        bitmap.Dispose();
+        return rotatedBitmap;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
